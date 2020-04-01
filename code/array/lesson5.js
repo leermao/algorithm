350de3b3e28825b85ada5a9e3f47fdd8d08ebdd0
lipeng1 <447552708@qq.com>
2020-04-01 09:45:27 +0800

    优化螺旋矩阵

--- code/array/lesson5.js
+++ code/array/lesson5.js
@@ -1,21 +1,33 @@
 /**
- * 环形打印数组内容
+ * @param {number[][]} matrix
+ * @return {number[]}
+ *
+ * 螺旋矩阵
+ *
+ * https://leetcode-cn.com/problems/spiral-matrix/
  */
-var ringArr = function (arr) {
+var spiralOrder = function (arr) {
+  if (arr.length === 0) return []
   let res
 
   while (arr.length) {
     res = res ? res.concat(arr.shift()) : arr.shift()
+    arr = arr.filter(item => item.length)
     for (let i = 0; i < arr.length; i++) {
-      res.push(arr[i].pop())
+      const pop = arr[i].pop()
+      res.push(pop)
     }
+    arr = arr.filter(item => item.length)
     res = res.concat((arr.pop() || []).reverse())
-    for (let index = arr.length - 1; index >= 0; index--) {
-      res.push(arr[index].shift())
+    arr = arr.filter(item => item.length)
+    if (arr.length) {
+      for (let index = arr.length - 1; index >= 0; index--) {
+        res.push(arr[index].shift())
+      }
     }
+    arr = arr.filter(item => item.length)
   }
 
   return res
 }
-
-export default ringArr
+export default spiralOrder
