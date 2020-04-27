6bc3c27562e56d3679a3b6b1eac9c5bf08dc0323
lipeng1 <447552708@qq.com>
2020-04-27 09:56:57 +0800

    链表中倒数第k个节点

---
+++ code/chain/lesson4.js
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ * 面试题22. 链表中倒数第k个节点
+ *
+ * https://leetcode-cn.com/problems/lian-biao-zhong-dao-shu-di-kge-jie-dian-lcof/
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+
+var getKthFromEnd = function (head, k) {
+  let firstHead = head;
+  let secondHead = head;
+
+  if (head.val === null) {
+    return null;
+  }
+
+  for (let i = 0; i < k; i++) {
+    if (secondHead === null) {
+      return null;
+    } else {
+      secondHead = secondHead.next;
+    }
+  }
+
+  while (secondHead) {
+    secondHead = secondHead.next;
+    firstHead = firstHead.next;
+  }
+
+  return firstHead;
+};
+
+export default getKthFromEnd;
